b1686c680d09513b9c981856c456317f8ce21b38
Rick Mur <rick@mur.nl>
2015-08-14 18:08:38 +0200

    Second commit, updated Exception handling

--- pushjinja.py
+++ pushjinja.py
@@ -4,28 +4,14 @@ __author__ = "rickmur"
 from jnpr.junos import Device
 from jnpr.junos.version import VERSION
 from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import CommitError
+from jnpr.junos.exception import ConfigLoadError
+from jnpr.junos.exception import ConnectAuthError
+from jnpr.junos.exception import RpcTimeoutError
 import yaml
-import sys
+
 #temp
 from getpass import getpass
-from pprint import pprint
-
-
-print('This is PyEZ version %s' % (VERSION))
-
-
-
-# ask for inputs
-uid = raw_input("Username? ")
-pwd = getpass("Password? ")
-
-
-# start setting it up
-dev = Device('172.22.1.3', user = uid, password = pwd)
-dev.open()
-#pprint(dev.facts)
-print('the JUNOS version of this device is %s' % (dev.facts["version"]))
-
 
 
 
@@ -33,25 +19,41 @@ print('the JUNOS version of this device is %s' % (dev.facts["version"]))
 
 try:
 
-  con = Config(dev)
+    print('This is PyEZ version %s' % (VERSION))
 
-  # load YML variables
-  myFile = open('data.yml')
-  data = yaml.load(myFile.read())
+    # ask for inputs
+    uid = raw_input("Username? ")
+    pwd = getpass("Password? ")
 
-  con.load(template_path='template.conf', template_vars=data)
-  if (con.commit()):
-    print 'SUCCESSSS'
-  else:
-    print 'whoops'
 
+    # start setting it up
+    dev = Device('172.22.1.3', user = uid, password = pwd)
+    dev.open()
+    #pprint(dev.facts)
+    print('the JUNOS version of this device is %s' % (dev.facts["version"]))
+
+    con = Config(dev)
+
+    # load YML variables
+    myFile = open('data.yml')
+    data = yaml.load(myFile.read())
+
+    con.load(template_path='template.conf', template_vars=data)
+    if (con.commit()):
+        print 'SUCCESSSS'
+    else:
+        print 'whoops'
+
+    dev.close()
+
+except ConnectAuthError:
+    print 'Credentials invalid!'
+except ConfigLoadError as e:
+    print 'Config template not correct!!!: ', e
+except CommitError as e:
+    print 'Config could not commit: ', e
+except RpcTimeoutError as e:
+    print 'Config committed, but RPC timed out: ', e
 except Exception as e:
-  print 'Exception caught you: ', e
-except:
-  print 'something went wrong: ', sys.exc_info()[0]
+    print 'something went wrong: ', e
 
-
-
-
-
-dev.close()
