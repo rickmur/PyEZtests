dabd86de7d08a4282bbefeb5f03f1d9eaa0a437a
Rick Mur <rick@mur.nl>
2015-11-17 16:00:48 +0100

    Changes to be committed: 	new file:   bgp.py new file:   bgp.yml 	new file:   bgpTable.py 	modified:   lldp.py 	modified:   switchlist.yml

--- bgpTable.py
+++ bgpTable.py
@@ -1,9 +1,13 @@
 # Rick Mur - Juniper - 2015
 
 from jnpr.junos import Device
-from jnpr.junos.op.lldp import LLDPNeighborTable
+from jnpr.junos.op.bgp import BGPNeighborTable as BGPtable
+
+
 import yaml
 from pprint import pprint as pp
+
+
 
 myVars = yaml.load(open("switchlist.yml").read())
 
@@ -11,13 +15,17 @@ for switch in myVars["switches"]:
     dev = Device(switch, user="pytraining", password="Poclab123")
     dev.open()
 
-    neighbors = LLDPNeighborTable(dev)
+    neighbors = BGPtable(dev)
     neighbors.get()
 
     print ("Connected to " + dev.hostname)
     if len(neighbors) != 0:
         for neighbor in neighbors:
-            print ("Interface " + neighbor.local_int + " is connecting to " + neighbor.remote_sysname)
+            peer = neighbor.neighbor.split("+")
+            initiate = "locally"
+            if peer[1] != "179":
+                initiate = "by peer"
+            print ("Neighbor " + peer[0] + " is AS " + neighbor.peer_as + " state is: " + neighbor.state + " session was initiated " + initiate)
     else:
         print ("No neighbors")
 
