b49819965dca8f28add2c08ba204e0ca6ae086a3
Rick Mur <rick@mur.nl>
2015-11-17 15:11:20 +0100

    Changes to be committed: 	modified:   lldp.py

--- lldp.py
+++ lldp.py
@@ -1,4 +1,24 @@
 # Rick Mur - Juniper - 2015
 
 from jnpr.junos import Device
+from jnpr.junos.op.lldp import LLDPNeighborTable
 import yaml
+from pprint import pprint as pp
+
+myVars = yaml.load(open("switchlist.yml").read())
+
+for switch in myVars["switches"]:
+    dev = Device(switch, user="pytraining", password="Poclab123")
+    dev.open()
+
+    neighbors = LLDPNeighborTable(dev)
+    neighbors.get()
+
+    print ("Connected to " + dev.hostname)
+    if len(neighbors) != 0:
+        for neighbor in neighbors:
+            print ("Interface " + neighbor.local_int + " is connecting to " + neighbor.remote_sysname)
+    else:
+        print ("No neighbors")
+
+
